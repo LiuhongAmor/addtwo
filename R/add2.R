9257fba9e55bf464809c62b8644b1dbc96c63573
LiuhongAmor <48950916+LiuhongAmor@users.noreply.github.com>
2019-03-27 21:27:36 +0800

    Add files via upload

---
+++ R/add2.R
@@ -0,0 +1,11 @@
+#'add2
+#'
+#'add two num together
+#'@param x a num
+#'@param y a num
+#'@example add2(1,100)
+#'
+#'
+add2<- function(x,y) {
+  x+y
+}
---
+++ R/addtwo.R
@@ -0,0 +1,10 @@
+#'add2
+#'
+#'这个包是用于两个数相加
+#'@section add2 Function
+#'add2 Functions 有连个参数
+#'@docType package
+#'@name addtwo
+
+NULL
+
